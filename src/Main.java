06216512f754f8a1c644d26721d353a23285e560
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2024-12-12 10:42:04 +0100

    hej igen

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.InputMismatchException;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         int nummer = 5;
-        boolean login = false;
+
+
+        /*boolean login = false;
 
         if (nummer <= 6 ||nummer >= 3){
             System.out.println("Hello world!");
@@ -21,7 +25,7 @@ public class Main {
         String password = in.nextLine();
         System.out.println("Password is " + password);
 
-        /*
+
         if (Objects.equals(username, "noname")&&Objects.equals(password, "noname")){
             System.out.println("Welcome");
             login = true;
@@ -31,7 +35,7 @@ public class Main {
         }
         for (int i = 0; i < 32; i++){
             System.out.println("Hello world (:");
-        }*/
+        }
 
         while (login == true){
             System.out.println("Do you want to login again?");
@@ -43,6 +47,48 @@ public class Main {
                 System.out.println("Högre än 5!");
             }
         }
+
+        boolean moron = true;
+        Scanner sc = new Scanner(System.in);
+        while (moron){
+            try {
+                System.out.println("Enter number");
+                int number = sc.nextInt();
+                moron = false;
+            }
+            catch (InputMismatchException e){
+                sc.nextLine();
+                System.out.println("A number!");
+            }
+        }*/
+
+        boolean findnum = true;
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int random = rand.nextInt(101);
+
+        while (findnum) {
+            try {
+                System.out.println("Guess what number im thinking of");
+                int guess = scan.nextInt();
+
+                if (guess == random) {
+                    System.out.println(":)");
+                    findnum = false;
+                }
+                else if (guess < random) {
+                    System.out.println("Try a higher number haha");
+                }
+                else if (guess > random) {
+                    System.out.println("Try a lower number haha");
+                }
+            }
+            catch (InputMismatchException e) {
+                scan.nextLine();
+                System.out.println("Dum! Try again");
+            }
+        }
+
     }
 
 }
